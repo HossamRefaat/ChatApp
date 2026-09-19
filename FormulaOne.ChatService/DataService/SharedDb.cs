9a3fa5f5f3e5df664bed1053b6a6fe26b319875b
HossamRefaat <hossamrefaatmazmaz17@gmail.com>
2026-09-19 21:03:43 +0300

    initial

---
+++ FormulaOne.ChatService/DataService/SharedDb.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Concurrent;
+using FormulaOne.ChatService.Model;
+namespace FormulaOne.ChatService.DataService;
+
+public class SharedDb
+{
+    public readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+
+    public ConcurrentDictionary<string, UserConnection> Connections => _connections;
+}
---
+++ FormulaOne.ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using FormulaOne.ChatService.DataService;
+using FormulaOne.ChatService.Model;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FormulaOne.ChatService.Hubs;
+
+public class ChatHub : Hub
+{
+
+    private readonly SharedDb _shared;
+
+    public ChatHub(SharedDb shared) => _shared = shared;
+
+    public async Task JoinRoom(UserConnection conn)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", $"admin", $"{conn.UserName} has joined the room");
+    }
+
+    public async Task JoinSpecificChatRoom(UserConnection conn)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+        _shared.Connections[Context.ConnectionId] = conn;
+        await Clients.Group(conn.ChatRoom).SendAsync("JoinSpecificChatRoom", $"admin", $"{conn.UserName} has joined the room {conn.ChatRoom}");
+    }
+
+    public async Task SendMessage(string msg)
+    {
+        if(_shared.Connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+        {
+            await Clients.Group(conn.ChatRoom)
+                .SendAsync("ReceiveSpecificMessage", conn.UserName, msg);
+        }
+    }
+}
